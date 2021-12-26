584eccb75576747dfbdb7a13eab87618aa8a9227
linling <linling199200@gmail.com>
2021-12-26 15:21:42 -0700

    added comments and image

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 admin.site.register(Post)
+admin.site.register(Comment)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'text', 'post', 'created_on', 'approved_comment')
+    list_filter = ('approved_comment', 'created_on')
+    search_fields = ('author', 'email', 'text')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,34 @@
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404, redirect
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 def post_detail(request, pk):
+    template_name = 'blog/post_detail.html'
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    comments = post.comments.filter(approved_comment=True)
+    new_comment = None
+    # Comment posted
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, template_name, {'post': post,
+                                           'comments': comments,
+                                           'new_comment': new_comment,
+                                           'comment_form': comment_form})
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
